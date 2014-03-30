12a60a379f2c94dfaadc3354f017c8eaa3704b1a
Evgen <osben90@gmail.com>
2014-03-30 12:29:01 +0300

    баг с лимитом по умолчанию
    Больше 100 не будет выводить, потому как будет срабатывать лимит по умолчанию. Поэтому нужно принудительно передавать лимит

--- view/ProductView.php
+++ view/ProductView.php
@@ -109,7 +109,7 @@ class ProductView extends View
 		}
 		if(!empty($related_ids))
 		{
-			foreach($this->products->get_products(array('id'=>$related_ids, 'in_stock'=>1, 'visible'=>1)) as $p)
+			foreach($this->products->get_products(array('id'=>$related_ids, 'in_stock'=>1, 'visible'=>1, 'limit'=>count($related_ids))) as $p)
 				$related_products[$p->id] = $p;
 			
 			$related_products_images = $this->products->get_images(array('product_id'=>array_keys($related_products)));
