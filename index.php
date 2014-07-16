10818494f608ea7da0ab48bf425b1a313d1217cd
Evgen <osben90@gmail.com>
2014-07-16 11:44:31 +0300

    баг, 404 сохраняло в последний посещенную страницу

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if(($res = $view->fetch()) !== false)
 	print $res;
 
 	// Сохраняем последнюю просмотренную страницу в переменной $_SESSION['last_visited_page']
-	if(empty($_SESSION['last_visited_page']) || empty($_SESSION['current_page']) || $_SERVER['REQUEST_URI'] !== $_SESSION['current_page'])
+	if((empty($_SESSION['last_visited_page']) || empty($_SESSION['current_page']) || $_SERVER['REQUEST_URI'] !== $_SESSION['current_page']) && ($_GET['page_url'] != '404' && $_GET['module'] != 'PageView'))
 	{
 		if(!empty($_SESSION['current_page']) && !empty($_SESSION['last_visited_page']) && $_SESSION['last_visited_page'] !== $_SESSION['current_page'])
 			$_SESSION['last_visited_page'] = $_SESSION['current_page'];
