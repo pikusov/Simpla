78d791a075edf3596ab2e2b554e2f3642b974d88
Erko Evgen <osben90@gmail.com>
2016-01-06 11:01:47 +0200

    fix 404 & detect image type
    1. при не правильно token добавлен заголовок 404
    2. при генерации добавленно определение типа рисунка и передача его в
    заголовок

--- resize/resize.php
+++ resize/resize.php
@@ -7,14 +7,31 @@ $token = $_GET['token'];
 
 $simpla = new Simpla();
 
-if(!$simpla->config->check_token($filename, $token))
-	exit('bad token');		
 
-$resized_filename =  $simpla->image->resize($filename);
+if(!$simpla->config->check_token($filename, $token))
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 404 Not Found");
+	exit('bad token');
+}
+
+$resized_filename = $simpla->image->resize($filename);
 
 if(is_readable($resized_filename))
 {
-	header('Content-type: image');
+	if (function_exists('exif_imagetype')) {
+		$image_type = exif_imagetype( $resized_filename );
+	} else {
+		$image_type = getimagesize( $resized_filename );
+		$image_type = ( isset($image_type[2]) ) ? $image_type[2] : NULL;
+	}
+
+	if( is_null($image_type) )
+		$image_mime = 'image';
+	else
+		$image_mime = image_type_to_mime_type($image_type);
+
+	header("Content-type: ".$image_mime);
 	print file_get_contents($resized_filename);
+	exit;
 }
 
