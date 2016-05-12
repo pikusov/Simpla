87bbd6b7a7aab46f6306a36b3b597dc0da26dfaf
Erko Evgen <osben90@gmail.com>
2016-05-13 02:54:43 +0300

    fix

--- api/Managers.php
+++ api/Managers.php
@@ -35,7 +35,7 @@ class Managers extends Simpla
 
 	public function get_managers()
 	{
-		$lines = explode("\n", @file_get_contents(dirname(dirname(__FILE__)).'/'.$this->passwd_file));
+		$lines = explode("\n", @file_get_contents($this->passwd_file));
 		$managers = array();
 		foreach($lines as $line)
 		{
